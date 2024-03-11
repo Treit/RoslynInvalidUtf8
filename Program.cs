5ec78fef6df96c9db5aeb3f28912ca71fee205b2
Mike Treit <mtreit@ntdev.microsoft.com>
2024-03-10 17:48:09 -0700

    Show invalid utf-8 chars in source.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+ï»¿namespace Test
+{
+    class Program
+    {
+        private static readonly Dictionary<string, int> dict = new Dictionary<string, int>
+        {
+            {"intlv3_pokã©mon", 1216},
+            {"intlv3_pokémon", 1248}
+        };
+
+        static void Main(string[] args)
+        {
+            foreach (var item in dict)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
